fa9d4754daadaf47bc65f1c5e80d0c42812ea66f
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-12-27 18:50:07 +0800

    initial

---
+++ 1_exploration.R
@@ -0,0 +1,9 @@
+setwd('/Users/ivanliu/Downloads/Prudential-Life-Insurance-Assessment')
+rm(list=ls()); gc()
+library(data.table)
+train <- fread('data/train.csv')
+test <- fread('data/test.csv')
+submission <- fread('data/sample_submission.csv')
+
+# 1. basic 
+dim(train);dim(test)
