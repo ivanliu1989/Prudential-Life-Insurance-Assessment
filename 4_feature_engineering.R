776f66ad8560be73a6e6ba7b1f73e4eb89a3aa01
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2016-01-24 23:16:08 +1100

    DATA EXPLORE

--- 4_feature_engineering.R
+++ 4_feature_engineering.R
@@ -168,8 +168,70 @@ table(train$Response) # Categorical - 8
 # 3. NA value & Imputation #####
 ################################
 sapply(names(train), function(x){mean(is.na(train[,x]))})
-feature.names <- names(train)[2:ncol(train)-1]
+na.features <- c('Employment_Info_1', 'Employment_Info_4', 'Employment_Info_6',
+                 'Insurance_History_5', 'Family_Hist_2', 'Family_Hist_3', 'Family_Hist_4', 'Family_Hist_5',
+                 'Medical_History_1', 'Medical_History_10', 'Medical_History_15', 'Medical_History_24', 'Medical_History_32')
+cate.features <- c("Product_Info_1", "Product_Info_2", "Product_Info_3", "Product_Info_5", "Product_Info_6", "Product_Info_7", 
+                   "Employment_Info_2", "Employment_Info_3", "Employment_Info_5", 
+                   "InsuredInfo_1", "InsuredInfo_2", "InsuredInfo_3", "InsuredInfo_4", "InsuredInfo_5", "InsuredInfo_6", "InsuredInfo_7", 
+                   "Insurance_History_1", "Insurance_History_2", "Insurance_History_3", "Insurance_History_4", "Insurance_History_7", 
+                   "Insurance_History_8", "Insurance_History_9", 
+                   "Family_Hist_1", "Medical_History_2", 
+                   "Medical_History_3", "Medical_History_4", "Medical_History_5", "Medical_History_6", "Medical_History_7", 
+                   "Medical_History_8", "Medical_History_9", "Medical_History_11", "Medical_History_12", #Medical_History_10
+                   "Medical_History_13", "Medical_History_14", "Medical_History_16", "Medical_History_17", "Medical_History_18", 
+                   "Medical_History_19", "Medical_History_20", "Medical_History_21", "Medical_History_22", "Medical_History_23", 
+                   "Medical_History_25", "Medical_History_26", "Medical_History_27", "Medical_History_28", "Medical_History_29", 
+                   "Medical_History_30", "Medical_History_31", "Medical_History_33", "Medical_History_34", "Medical_History_35", 
+                   "Medical_History_36", "Medical_History_37", "Medical_History_38", "Medical_History_39", "Medical_History_40", "Medical_History_41")
+medical.keywords <- c(paste('Medical_Keyword_', 1:48, sep = ''))
 
-################################
-# 4. tsne based on sectors #####
-################################
+head(train[,na.features]) # all Numerical (Continuous) or Numerical (Discrete) - 240 
+
+test$Response <- 9
+total <- rbind(train, test); dim(total)
+
+#############################
+# 5. One - hot encoding #####
+#############################
+for(i in cate.features){
+    total[,i] <- as.factor(total[,i])
+}
+str(total)
+dummies <- data.frame(model.matrix(~.-1,total[,cate.features]))
+head(dummies)
+
+total_new <- cbind(total[,-which(names(total) %in% c(cate.features,'Response'))], dummies, Response = total$Response)
+
+#######################
+# 6. New Features #####
+#######################
+# i. Count of Medical Keywords
+Cnt_Medi_Key_Word <- rowSums(total[, medical.keywords])
+
+# ii. product of BMI and Ins_Age
+BMI_InsAge <- total$BMI * total$Ins_Age
+
+# iii. number of NA's
+Cnt_NA_row <- apply(total, 1, function(x) sum(is.na(x)))
+
+# iv. get rid of Medical_History_10 and Medical_History_24
+
+#####################
+# 7. Imputation #####
+#####################
+for(i in na.features){
+    total[is.na(total[,i]),i] <- median(total[,i], na.rm = T)
+}
+sapply(names(total), function(x){mean(is.na(total[,x]))})
+
+#########################################
+# 8. kmeans & tsne based on sectors #####
+#########################################
+
+
+#####################
+# 9. Re-combine #####
+#####################
+total_new <- cbind(total[,-which(names(total) %in% c(cate.features,'Response'))], dummies, Response = total$Response)
+
