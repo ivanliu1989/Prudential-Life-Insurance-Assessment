75b138e013c892930b447666045051a176c4deb9
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2016-01-31 22:29:04 +1100

    h2o

--- Rscripts/iv_submission_xgb.R
+++ Rscripts/iv_submission_xgb.R
@@ -158,7 +158,7 @@ clf <- xgb.train(data = dtrain_a, eval_metric = 'mlogloss',
                  booste = "gbtree", eta = 0.035, max_depth = 6, min_child_weight = 3, subsample = 0.9,
                  nrounds = 800, colsample = 0.67, print.every.n = 10 ,num_class = 8
 )
-pred_b_softprob <- predict(clf, data.matrix(train_b[,feature.names]))
+pred_b_softprob_mlog <- predict(clf, data.matrix(train_b[,feature.names]))
 clf <- xgb.train(data = dtrain_b, eval_metric = 'mlogloss',
                  early.stop.round = 200, watchlist = watchlist_ba, maximize = F, 
                  verbose = 1, objective = "multi:softprob",
@@ -180,18 +180,18 @@ train_2nd$XGB_RMSE <- c(pred_a_rmse, pred_b_rmse)
 train_2nd$XGB_KAPPA <- c(pred_a_kappa, pred_b_kappa)
 train_2nd$XGB_SOFTMAX <- c(pred_a_softmax, pred_b_softmax)
 train_2nd$XGB_SOFTMAX_MLOG <- c(pred_a_softmax_mlog, pred_b_softmax_mlog)
-META_XGB_MULSOFT <- as.data.frame(rbind(matrix(pred_a_softprob, ncol=8), matrix(pred_b_softprob, ncol=8)))
+META_XGB_MULSOFT <- as.data.frame(rbind(t(matrix(pred_a_softprob, 8, nrow(train_a))), t(matrix(pred_b_softprob, 8, nrow(train_b)))))
 names(META_XGB_MULSOFT) <- paste('META_XGB_MUL_', 1:8, sep = '')
-META_XGB_MULSOFT_MLOG <- as.data.frame(rbind(matrix(pred_a_softprob_mlog, ncol=8), matrix(pred_b_softprob_mlog, ncol=8)))
+META_XGB_MULSOFT_MLOG <- as.data.frame(rbind(t(matrix(pred_a_softprob_mlog,  8, nrow(train_a))), t(matrix(pred_b_softprob_mlog, 8, nrow(train_b)))))
 names(META_XGB_MULSOFT_MLOG) <- paste('META_XGB_MUL_MLOG_', 1:8, sep = '')
 train_2nd <- cbind(train_2nd, META_XGB_MULSOFT, META_XGB_MULSOFT_MLOG)
 
 test$XGB_RMSE <- pred_test_rmse
 test$XGB_KAPPA <- pred_test_kappa
 test$XGB_SOFTMAX <- pred_test_softmax
-META_XGB_MULSOFT <- as.data.frame(matrix(pred_test_softprob, ncol=8))
+META_XGB_MULSOFT <- as.data.frame(t(matrix(pred_test_softprob, 8, nrow(test))))
 names(META_XGB_MULSOFT) <- paste('META_XGB_MUL_', 1:8, sep = '')
-META_XGB_MULSOFT_MLOG <- as.data.frame(matrix(pred_test_softprob_mlog, ncol=8))
+META_XGB_MULSOFT_MLOG <- as.data.frame(t(matrix(pred_test_softprob_mlog, 8, nrow(test))))
 names(META_XGB_MULSOFT_MLOG) <- paste('META_XGB_MUL_MLOG_', 1:8, sep = '')
 test <- cbind(test, META_XGB_MULSOFT, META_XGB_MULSOFT_MLOG)
 
@@ -199,9 +199,9 @@ train_a_2nd <- train_a
 train_a_2nd$XGB_RMSE <- pred_a_rmse
 train_a_2nd$XGB_KAPPA <- pred_a_kappa
 train_a_2nd$XGB_SOFTMAX <- pred_a_softmax
-META_XGB_MULSOFT <- as.data.frame(matrix(pred_a_softprob, ncol=8))
+META_XGB_MULSOFT <- as.data.frame(t(matrix(pred_a_softprob, 8, nrow(train_a))))
 names(META_XGB_MULSOFT) <- paste('META_XGB_MUL_', 1:8, sep = '')
-META_XGB_MULSOFT_MLOG <- as.data.frame(matrix(pred_a_softprob_mlog, ncol=8))
+META_XGB_MULSOFT_MLOG <- as.data.frame(t(matrix(pred_a_softprob_mlog, 8, nrow(train_a))))
 names(META_XGB_MULSOFT_MLOG) <- paste('META_XGB_MUL_MLOG_', 1:8, sep = '')
 train_a_2nd <- cbind(train_a_2nd, META_XGB_MULSOFT, META_XGB_MULSOFT_MLOG)
 
@@ -209,10 +209,19 @@ train_b_2nd <- train_b
 train_b_2nd$XGB_RMSE <- pred_b_rmse
 train_b_2nd$XGB_KAPPA <- pred_b_kappa
 train_b_2nd$XGB_SOFTMAX <- pred_b_softmax
-META_XGB_MULSOFT <- as.data.frame(matrix(pred_b_softprob, ncol=8))
+META_XGB_MULSOFT <- as.data.frame(t(matrix(pred_b_softprob, 8, nrow(train_b))))
 names(META_XGB_MULSOFT) <- paste('META_XGB_MUL_', 1:8, sep = '')
-META_XGB_MULSOFT_MLOG <- as.data.frame(matrix(pred_b_softprob_mlog, ncol=8))
+META_XGB_MULSOFT_MLOG <- as.data.frame(t(matrix(pred_b_softprob_mlog, 8, nrow(train_b))))
 names(META_XGB_MULSOFT_MLOG) <- paste('META_XGB_MUL_MLOG_', 1:8, sep = '')
 train_b_2nd <- cbind(train_b_2nd, META_XGB_MULSOFT, META_XGB_MULSOFT_MLOG)
 
 save(train_2nd, train_a_2nd, train_b_2nd, test, file= 'data/fin_train_test_stack_feat.RData')
+
+ScoreQuadraticWeightedKappa(round(pred_b_rmse),train_b$Response)
+ScoreQuadraticWeightedKappa(round(pred_a_rmse),train_a$Response)
+ScoreQuadraticWeightedKappa(round(pred_b_kappa),train_b$Response)
+ScoreQuadraticWeightedKappa(round(pred_a_kappa),train_a$Response)
+ScoreQuadraticWeightedKappa(round(pred_b_softmax),train_b$Response)
+ScoreQuadraticWeightedKappa(round(pred_a_softmax),train_a$Response)
+ScoreQuadraticWeightedKappa(round(pred_b_softmax_mlog),train_b$Response)
+ScoreQuadraticWeightedKappa(round(pred_a_softmax_mlog),train_a$Response)
