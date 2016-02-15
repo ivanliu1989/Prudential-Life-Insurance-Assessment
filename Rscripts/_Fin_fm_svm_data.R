4e5131a963d292d2f78a91e1267f7c1aa4f6abe0
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2016-02-15 19:47:09 +1100

    update

---
+++ Rscripts/_Fin_fm_svm_data.R
@@ -0,0 +1,52 @@
+setwd('/Users/ivanliu/Downloads/Prudential-Life-Insurance-Assessment')
+rm(list=ls());gc()
+load('data/xgb_meta_leaf_20150211_dummy.RData')
+load('../final_regression_pca.RData')
+
+options(scipen=999);set.seed(19890624)
+
+head(train)
+
+ffmlib_convert <- function(dt){
+    # dropitems <- c('Id','Response')
+    # feature.names <- names(train)[!names(train) %in% dropitems] 
+    # dt <- dt[,feature.names]
+    for(i in 2:(ncol(dt)-1)){
+        print(i)
+        if(i==2){
+            dt$ffm <- paste0(dt$Response,' ',i-1,':', i-1,':',dt[,i])
+        }else{
+            dt$ffm <- paste0(dt$ffm,' ',i-1,':', i-1,':',dt[,i])
+        }   
+    }
+    return(dt$ffm)
+}
+
+
+train_ffm <- ffmlib_convert(train)
+test_ffm <- ffmlib_convert(test)
+
+write.table(train_ffm, file='../train_ffm.txt', quote = F, row.names = F, col.names = F)
+write.table(test_ffm, file='../test_ffm.txt', quote = F, row.names = F, col.names = F)
+
+
+svmlib_convert <- function(dt){
+    dt <- dt[,c(58, 3:57)]
+    # dt <- dt[,c(37, 3:35)]
+    dt$flag_class <- ifelse(dt$flag_class == 'Y', 1, 0)
+    for(i in 2:ncol(dt)){
+        print(i)
+        if(i==2){
+            dt$ffm <- paste0(dt$flag_class,' ',i-1,':',dt[,i])
+        }else{
+            dt$ffm <- paste0(dt$ffm,' ',i-1,':',dt[,i])
+        }   
+    }
+    return(dt$ffm)
+}
+
+train_svm <- svmlib_convert(train)
+test_svm <- svmlib_convert(test)
+
+write.table(train_svm, file='../train_svm.txt', quote = F, row.names = F, col.names = F)
+write.table(test_svm, file='../test_svm.txt', quote = F, row.names = F, col.names = F)
--- submission_final.R
+++ submission_final.R
@@ -123,7 +123,7 @@ independent <- paste0('var_', 1:length(independent))
 train_df          <- as.h2o(localH2O, train)
 validation_df     <- as.h2o(localH2O, test)
 
-for(i in 11:20){
+for(i in 21:50){
     set.seed(i*66)
     cat(paste0('Start training Deep Learning...', i))
     fit <-
@@ -135,7 +135,7 @@ for(i in 11:20){
             nesterov_accelerated_gradient = T, input_dropout_ratio = 0.5, hidden_dropout_ratios = c(0.5,0.5), 
             l1 = 1e-5, l2 = 3e-5, loss = 'MeanSquare', classification_stop = 0.01,
             diagnostics = T, variable_importances = F, fast_mode = F, ignore_const_cols = T,
-            force_load_balance = F, replicate_training_data = F, shuffle_training_data = T
+            force_load_balance = T, single_node_mode = T, shuffle_training_data = T
         )
     validPreds <- as.data.frame(h2o.predict(object = fit, newdata = validation_df))
     # fix_cut <- c(2.951759, 3.653780, 4.402781, 4.911808, 5.543988, 6.135754, 6.716891)
@@ -148,8 +148,49 @@ for(i in 11:20){
 
 ### ordinal regression
 library(VGAM)
+load('../final_regression_pca.RData')
 # Logistics regression Using Cumulative Logits
 fit <- vglm(as.factor(Response) ~ . , family=cumulative(parallel=TRUE), data=train[,-1]) # Logistics regression Using Cumulative Logits
 p1_r <- predict(fit, type = "response", test)
 p1_t <- max.col(p1_r)
+
+
+
+# dummy
+cate.features <- c("Product_Info_1", "Product_Info_3", "Product_Info_5", "Product_Info_6", "Product_Info_7", 
+                   "Employment_Info_2", "Employment_Info_3", "Employment_Info_5", 
+                   "InsuredInfo_1", "InsuredInfo_2", "InsuredInfo_3", "InsuredInfo_4", "InsuredInfo_5", "InsuredInfo_6", "InsuredInfo_7", 
+                   "Insurance_History_1", "Insurance_History_2", "Insurance_History_3", "Insurance_History_4", "Insurance_History_7", 
+                   "Insurance_History_8", "Insurance_History_9", 
+                   "Family_Hist_1", "Medical_History_2", 
+                   "Medical_History_3", "Medical_History_4", "Medical_History_5", "Medical_History_6", "Medical_History_7", 
+                   "Medical_History_8", "Medical_History_9", "Medical_History_11", "Medical_History_12",
+                   "Medical_History_13", "Medical_History_14", "Medical_History_16", "Medical_History_17", "Medical_History_18", 
+                   "Medical_History_19", "Medical_History_20", "Medical_History_21", "Medical_History_22", "Medical_History_23", 
+                   "Medical_History_25", "Medical_History_26", "Medical_History_27", "Medical_History_28", "Medical_History_29", 
+                   "Medical_History_30", "Medical_History_31", "Medical_History_33", "Medical_History_34", "Medical_History_35", 
+                   "Medical_History_36", "Medical_History_37", "Medical_History_38", "Medical_History_39", "Medical_History_40", 
+                   "Medical_History_41"
+                   ,'Product_Info_2_cate', 'Product_Info_2_num'
+)
+test$Response <- 0
+All_Data <- rbind(train,test) 
+for(i in cate.features){
+    if(mean(is.na(All_Data[,i]))==0){
+        All_Data[,i] <- as.factor(All_Data[,i])
+    }
+}
+dummies <- dummyVars(Response ~ ., data = All_Data, sep = "_", levelsOnly = FALSE, fullRank = TRUE)
+All_Data_d <- cbind(as.data.frame(predict(dummies, newdata = All_Data)), Response = All_Data$Response)
+
+library(caret)
+total_dum <- All_Data_d
+names(total_dum) <- c('Id', paste0('Var_', 1:(ncol(total_dum)-2)), 'Response')
+pcafit <- preProcess(as.matrix(total_dum[,2:(ncol(total_dum)-1)]), method = c('center','scale','pca'), thresh = 0.99, na.remove = TRUE)
+total_pca <- predict(pcafit, as.matrix(total_dum[,2:(ncol(total_dum)-1)]))
+total.pca <- cbind(Id = total_dum$Id, as.data.frame(total_pca), Response = total_dum$Response)
+train <- total.pca[total.pca$Response > 0,]
+test <- total.pca[total.pca$Response == 0,]
+save(train,test, file='../final_regression_pca.RData')
+
 
